db6c79a6df0522e4a7c2f05dae20d95b3e70c025
Jeffrey Lowy <jeffreylowy@gmail.com>
2017-12-18 20:18:49 -0800

    Squashed commit of the following:
    commit 15d95b413b9413053c8346b8fc2273c93e4473db
    Author: Jeffrey Lowy <jeffreylowy@gmail.com>
    Date:   Mon Dec 18 20:17:03 2017 -0800
    
        Recover from errors. Store all necessary data to variables.
    
    commit 175a3ebc105b6fce1474745e075201fa0b005be7
    Author: Jeffrey Lowy <jeffreylowy@gmail.com>
    Date:   Mon Dec 18 19:03:44 2017 -0800
    
        Get article ID and img url with request package
    
    commit 1c7ea7e1925bdd7cd97da2f3f19973f1483c1d8a
    Author: Jeffrey Lowy <jeffreylowy@gmail.com>
    Date:   Mon Dec 18 19:03:23 2017 -0800
    
        Swap classes on scrape button

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,12 +1,17 @@
 $(document).ready(function () {
 
 	let scrapeButton = $("nav a.btn.btn-floating");
+	let scrapeIcon = scrapeButton.find("i");
+
 	scrapeButton.hover(function () {
 		// Add pulse effect to scrape button
 		$(this).addClass("pulse");
+		$(this).addClass("appclr_neon_green");
+		scrapeIcon.addClass("appclr_black");
 	}, function () {
 		// Remove pulse effect to scrape button
-		$(this).removeClass("pulse");
+		$(this).removeClass("pulse, appclr_neon_green");
+		scrapeIcon.removeClass("appclr_black");
 	});
 
 });
